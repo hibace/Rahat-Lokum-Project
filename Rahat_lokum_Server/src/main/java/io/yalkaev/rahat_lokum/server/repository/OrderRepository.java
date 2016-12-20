59213b1114a5ac881d046139e909f3cd17631cc8
hibace <yalkaevnikolai@gmail.com>
2016-12-20 03:36:18 +0300

    fixed

--- Rahat_lokum_Server/src/main/java/io/yalkaev/rahat_lokum/server/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package io.yalkaev.rahat_lokum.server.repository;
-
-import io.yalkaev.rahat_lokum.server.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
